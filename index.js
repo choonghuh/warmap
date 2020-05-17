f8aed2053e7172ee0c9e55baab6bf9c49eb325d1
choong <choonghuh@gmail.com>
2020-05-17 12:53:55 -0700

    fixing port to enable Heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => console.log("listening at 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening at ${PORT}`));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
